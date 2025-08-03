0d6ec34f31f041f6053872b783af5555caf7508b
LuseBiswas <lusebiswas@gmail.com>
2025-08-03 19:36:49 +0530

    Resources Page is done

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,20 +1,15 @@
+import CTASection from "@/components/resources/CTASection";
 import HeroSection from "@/components/resources/HeroSection";
 import ResourceCard from "@/components/resources/resourceCard";
+import ResourcesCardItems from "@/components/resources/resourcesCardItems";
 
 
 export default function AboutPage() {
   return (
     <>
     <HeroSection />
-    <div className="bg-white p-14">
-    <ResourceCard
-  image="/image/screenshot_2.png"
-  title="Resource Title"
-  description="Brief description of the resource"
-  hoverButtonName="Learn More"
-/>
-    </div>
-
+    <ResourcesCardItems/>
+    <CTASection/>
     </>
     
   );
