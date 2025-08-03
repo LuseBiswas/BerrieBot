0f120bf62c63ce36af11243c7de04895b057d7ed
LuseBiswas <lusebiswas@gmail.com>
2025-08-03 13:02:09 +0530

    Footer and Navbar is separated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/home/NavBar";
-import Footer from "@/components/home/Footer";
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
