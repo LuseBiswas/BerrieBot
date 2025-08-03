460a89349f27dfe406cdcb8c0f3ded4d2f7a31a8
LuseBiswas <lusebiswas@gmail.com>
2025-08-03 21:58:42 +0530

    Blog Gradients is done

--- app/resources/explore/page.tsx
+++ app/resources/explore/page.tsx
@@ -1,5 +1,6 @@
 import CTASection from "@/components/resources/CTASection";
 import HeroSection from "@/components/resources/explore/HeroSection";
+import BlogSection from "@/components/resources/explore/blog";
 
 
 
@@ -7,7 +8,7 @@ export default function ExplorePage() {
   return (
     <>
     <HeroSection />
-    
+    <BlogSection />
     <CTASection/>
     </>
     
