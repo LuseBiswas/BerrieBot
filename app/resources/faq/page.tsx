610f6a1747ff7c4895831295f90371761d7018a5
LuseBiswas <lusebiswas@gmail.com>
2025-08-03 19:45:38 +0530

    FAQ section is initiated

---
+++ app/resources/faq/page.tsx
@@ -0,0 +1,15 @@
+import CTASection from "@/components/resources/CTASection";
+import HeroSection from "@/components/resources/faq/HeroSection";
+
+
+
+export default function ResourcePage() {
+  return (
+    <>
+    <HeroSection />
+    
+    <CTASection/>
+    </>
+    
+  );
+} 
---
+++ components/resources/faq/HeroSection.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <section className="relative mt-36 flex flex-col items-center justify-center px-4 sm:px-6 bg-transparent">
+      {/* ---- "We are Berribot" pill ---- */}
+      <div className="mb-12 relative z-10">
+        <div className="bg-[#00C7BEB2] text-white px-6 py-1 rounded-full font-inter font-medium text-lg">
+          FAQs
+        </div>
+      </div>
+
+      {/* ---- Main Heading ---- */}
+      <div className="text-center w-full max-w-5xl mx-auto relative z-10 mb-12">
+        <h1 className="font-inter text-[64px] sm:text-6xl md:text-7xl lg:text-8xl tracking-[-2px] sm:tracking-[-3.69px] mb-8 font-medium bg-gradient-to-b from-[#252527] to-[#ADADAEB0] text-transparent bg-clip-text">
+          You Asks, We
+          <br />
+          (and Berribot) Answer
+        </h1>
+      </div>
+
+      {/* ---- Description ---- */}
+      <div className="text-center w-full max-w-5xlxl mx-auto relative z-10">
+        <p className="font-inter text-[24px] sm:text-2xl md:text-[32px] leading-[1.4] sm:leading-[1.5] font-light text-white max-w-5xl mx-auto">
+        Got questions? We've got answers. If you're still wondering about something <br />
+         after this, just give us a shout—we're humans behind the bot, promise.
+        </p>
+      </div>
+    </section>
+  );
+}
