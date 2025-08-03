5eead421f8eaa0f33251ac49c3a32026759114b9
LuseBiswas <lusebiswas@gmail.com>
2025-08-03 14:35:34 +0530

    Privacy Police Page created

---
+++ app/policy/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/policy/HeroSection";
+import ValuesSection from "@/components/about/ValuesSection";
+
+export default function PolicyPage() {
+  return (
+    <>
+    <HeroSection />
+    <ValuesSection />
+    </>
+    
+  );
+} 
